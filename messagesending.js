efa9d808eb5e4eb3c8f79105454c590d6b7be0ee
Altaf94 <104971897+Altaf94@users.noreply.github.com>
2022-10-25 19:43:22 +0500

    rightnow

---
+++ messagesending.js
@@ -0,0 +1,28 @@
+const TodoList = () => {
+    const [todoList, setTodoList] = useState();
+    useEffect(() => {
+        const todoRef = firebase.database().ref('Todo');
+        todoRef.on('value', (snapshot) => {
+            const todos = snapshot.val();
+            const todoList = []
+            for (let id in todos) {
+                todoList.push({ id, ...todos[id] });
+            }
+            setTodoList(todoList);
+        })
+    }, [])
+    return (
+        <>
+            <h2>TodoList</h2>
+            <div
+                layout
+            >
+                {todoList ?
+                    todoList.map((todo, index) =>
+                        <Todo todo={todo} key={index} />
+                    )
+                    : ''}
+            </div>
+        </>
+    );
+}
